fb15476a80909a6a9106ce361b06e70e97eddb71
Hotdogmannen <hotdogmannen2@gmail.com>
2024-08-21 12:27:56 +0200

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,7 +1,8 @@
 project "imgui"
 	kind "StaticLib"
 	language "C++"
-    staticruntime "off"
+	cppdialect "C++20"
+    staticruntime "on"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -18,17 +19,15 @@ project "imgui"
 		"imstb_rectpack.h",
 		"imstb_textedit.h",
 		"imstb_truetype.h",
-		"imgui_demo.cpp"
+		"imgui_demo.cpp",
 	}
 
 	filter "system:windows"
 		systemversion "latest"
-		cppdialect "C++20"
 
 	filter "system:linux"
 		pic "On"
 		systemversion "latest"
-		cppdialect "C++20"
 
 	filter "configurations:Debug"
 		runtime "Debug"
